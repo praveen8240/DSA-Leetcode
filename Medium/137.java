e71e53271943b1605104148824482124163b55ae
praveen8240 <156305741+praveen8240@users.noreply.github.com>
2024-09-07 11:36:55 +0530

    137

---
+++ Medium/137.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0;
+        int twos = 0;
+        int threes = 0;
+        for (int num : nums) {
+            twos |= ones & num;
+            ones ^= num;
+            threes = ones & twos;
+            ones &= ~threes;
+            twos &= ~threes;
+        }
+        return ones;
+    }
+}
